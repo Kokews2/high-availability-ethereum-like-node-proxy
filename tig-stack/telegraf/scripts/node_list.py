d0860fe0bc47aa1a64fcc42369e0f5fc51139c39
Kokews2 <kokews2@gmail.com>
2024-05-03 11:11:13 +0200

    Test1 de TIG-Stack

---
+++ tig-stack/telegraf/scripts/node_list.py
@@ -0,0 +1,17 @@
+# SETTING NODE LIST
+
+nodes = [{
+    "id": 1,
+    "name": "Mainnet",
+    "url": "https://mainnet.infura.io/v3/ac5fa49e85494dd18f689dbf7e15505d"
+},
+{
+    "id": 2,
+    "name": "Sepolia",
+    "url": "https://sepolia.infura.io/v3/ac5fa49e85494dd18f689dbf7e15505d"
+},
+{
+    "id": 3,
+    "name": "Holesky",
+    "url": "https://holesky.infura.io/v3/ac5fa49e85494dd18f689dbf7e15505d"
+}]
