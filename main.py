8311cfa106ea8523773ee343bf116f3b3c0cb914
Kokews2 <kokews2@gmail.com>
2024-06-13 12:40:20 +0200

    Implementing Loki

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from scripts.python_scripts.node_list import nodes
 import requests
 import json
 import logging
-from colorama import init, Fore, Style
 
 app = FastAPI()
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-init(autoreset=True) # colorama settings
+# Logger settings
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='./tig-stack/alloy/logs/fastapi.log', filemode='a')
 
 @app.post("/receive_request")
 async def receive_request(request: Request):
@@ -19,12 +18,12 @@ async def receive_request(request: Request):
             efficient_node = json.load(file)
 
         response = requests.post(str(efficient_node["url"]), json=data)
-        logging.info(f'Request redirected to {Fore.GREEN}{efficient_node["name"]}{Style.RESET_ALL} node')
+        logging.info(f'Request successfuly redirected to {efficient_node["name"]} node')
 
         if response.status_code != 200:
-            logging.warning(f'Response not succed. HTTP status code: {Fore.RED}{response.status_code}{Style.RESET_ALL}')
+            logging.warning(f'Response not succed. HTTP status code: {response.status_code}')
 
         return response.json()
     except Exception as e:
-        logging.error(f'Error sending JSON-RPC request: {Fore.RED}{e}{Style.RESET_ALL}')
+        logging.error(f'Error sending JSON-RPC request: {str(e)}')
         return {"error": str(e)}
