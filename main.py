1cf9fbd4cd4680dab7679349123d7d5a4a1f123e
Kokews2 <kokews2@gmail.com>
2024-06-14 12:05:21 +0200

    Correct error in main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 app = FastAPI()
 
 # Logger settings
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='./tig-stack/alloy/logs/fastapi.log', filemode='a')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='./monitoring-stack/alloy/logs/fastapi.log', filemode='a')
 
 @app.post("/receive_request")
 async def receive_request(request: Request):
